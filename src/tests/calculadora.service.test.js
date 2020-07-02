7512a10a13aee57087401cfae55040d43f041ccb
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-07-02 14:56:09 -0300

    Criado testes para operacoes da calculadora

---
+++ src/tests/calculadora.service.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CalculadoraService from '../services/calculadora.service';
+
+describe('Casos de teste do CalculadoraService', () => {
+
+	const [ 
+		calcular,
+		SOMA,
+		SUBTRACAO,
+		DIVISAO,
+		MULTIPLICACAO
+	] = CalculadoraService();
+
+	it('Deve garantir que 1 + 4 = 5.', () => {
+		
+		let soma = calcular(1, 4, SOMA);
+		expect(soma).toEqual(5);
+	});
+
+	it('Deve garantir que 1 - 4 = -3.', () => {
+
+		let subtracao = calcular(1, 4, SUBTRACAO);
+		expect(subtracao).toEqual(-3);
+	});
+
+	it('Deve garantir que 1 / 4 = 0.25', () => {
+		
+		let divisao = calcular(1, 4, DIVISAO);
+		expect(divisao).toEqual(0.25);
+	});
+
+	it('Deve garantir que 1 * 4 = 4', () => {
+		
+		let multiplicacao = calcular(1, 4, MULTIPLICACAO);
+		expect(multiplicacao).toEqual(4);
+	});
+
+	it('Deve retornar 0 para operação inválida', () => {
+		
+		let operacaoInvalida = calcular(1, 4, '%');
+		expect(operacaoInvalida).toEqual(0);
+	})
+})
